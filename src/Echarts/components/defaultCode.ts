1ce09b3e21d9d4fd2e106f02bded7008a1e5bc51
尹春阳 <2646403766@qq.com>
2024-02-05 17:23:51 +0800

    feat:base64 代码

--- src/Echarts/components/defaultCode.ts
+++ src/Echarts/components/defaultCode.ts
@@ -1,168 +1,99 @@
-export const code:string = `const switchIcon = "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAAXNSR0IArs4c6QAAAUZJREFUWEftV0FugzAQ3JX8jyZnLxJPCD9JXpL2JU1ekj4BieWc/AMkt4tiBBaii0VLDubMrMdj784YYeMPN14fEoFeAWvtJwAcEHGnORbn3AMRr8aYS1mWDw1m6p+OABHdZPHIIh/M/B6JBczzfNe27d0XcM5dlMV6tYwx+1gVMMuyo3NO5JdPvZshDhFPVVVpiY/2NyKwpFAikBSYUoCIDnKh52ZJOD9WvYREJPPgrGzjruNWJfCcKdLWbzMkRvNjVQKanYdHlwgkBV5TAWmnpmluUwNF7Lqu61PsKFZ1wRyBn+DyxczFnxKI6WdtHlAp8K8ExEAAQDIhiFGIxBoCQYAtmFmFC/yi6EKptfauTcNT5JjZh9tf3XCIF5wHLnGxcaYb5MElbujjX/8ueB6F7GDOycLFr0PplW4I8p7wuPQ021yBb/YfzKoaOS2BAAAAAElFTkSuQmCC"
-let data = {
-   xdata: [
-      '0-4点', '4-8点', '8-12点', '12-16点', '16-20点', '20-0点'
-   ],
-   develop: [
-      100, 200, 300, 100, 150, 220
-   ],
-   complaint: [
-      50, 10, 15, 20, 25, 30
-   ],
-}
+let _str = `bGV0IGRhdGEgPSBbCiAgIHsKICAgICAgbmFtZTogJ+WSqOivouexu+aVsOmHjycsCiAgICAgIHZhbHVlOiA1MDAwLAogICAgICBwcm86IDUwCiAgIH0sCiAgIHsKICAgICAgbmFtZTogJ+aKleivieexu+aVsOmHjycsCiAgICAgIHZhbHVlOiAzMDAwLAogICAgICBwcm86IDMwCiAgIH0sCiAgIHsKICAgICAgbmFtZTogJ+ivhOS7t+exu+aVsOmHjycsCiAgICAgIHZhbHVlOiAyMDAwLAogICAgICBwcm86IDIwCiAgIH0KXQpjb25zdCBwdXNoWmVybyA9IChuKSA9PiB7CiAgIGlmIChuICUgMTAgPT0gMCkgewogICAgICByZXR1cm4gbiArICcuMDAnCiAgIH0gZWxzZSB7CiAgICAgIHJldHVybiBuCiAgIH0KfQpsZXQgY29sb3JzID0gWycjM0I4Q0U5JywgJyM1QkMzNzQnLCAnI0VBNjY3MiddOwpsZXQgZGF0YU5hbWVNYXAgPSB7CiAgICflkqjor6LnsbvmlbDph48nOiAnenhsJywKICAgJ+aKleivieexu+aVsOmHjyc6ICd0c2wnLAogICAn6K+E5Lu357G75pWw6YePJzogJ3BqbCcKfQpsZXQgbGVnZW5kUmljaHMgPSB7CiAgIHRpdGxlMTogewogICAgICBjb2xvcjogJyM5OTk5OTknLAogICAgICB3aWR0aDogMTIwLAogICAgICBhbGlnbjogJ2xlZnQnLAogICAgICBoZWlnaHQ6IDE0LAogICAgICBmb250U2l6ZTogMTQsCiAgICAgIHBhZGRpbmc6IFsyLCAwLCAwLCA0XQogICB9LAogICB0aXRsZTI6IHsKICAgICAgaGVpZ2h0OiAxNCwKICAgICAgY29sb3I6ICcjOTk5OTk5JywKICAgICAgZm9udFNpemU6IDE0LAogICAgICBwYWRkaW5nOiBbMiwgMCwgMCwgMF0KICAgfSwKICAgemhhbjogewogICAgICBoZWlnaHQ6IDE0CiAgIH0sCiAgIG5zZWw6IHsKICAgICAgd2lkdGg6IDE0LAogICAgICBoZWlnaHQ6IDE0LAogICAgICBiYWNrZ3JvdW5kQ29sb3I6ICcjY2NjY2NjOGEnLAogICAgICBib3JkZXJSYWRpdXM6IDE0LAogICB9LAogICBudmFsdWUxOiB7CiAgICAgIHdpZHRoOiAxMjAsCiAgICAgIGZvbnRTaXplOiAxOCwKICAgICAgY29sb3I6ICcjOTk5OTk5JywKICAgICAgcGFkZGluZzogWzAsIDAsIDAsIDE0ICsgNF0KICAgfSwKICAgbnZhbHVlMjogewogICAgICBmb250U2l6ZTogMTgsCiAgICAgIGNvbG9yOiAnIzk5OTk5OScsCiAgIH0KfTsKbGV0IHNlbGVjdHMgPSB7fTsKbGV0IGxlZ2VuZCA9IFtdOwpjb25zdCBjb21wdXRlZExlZ2VuZFJpY2hzID0gKERBVEEpID0+IHsKICAgREFUQS5tYXAoKGl0ZW0sIGluZGV4KSA9PiB7CiAgICAgIGxlZ2VuZC5wdXNoKGl0ZW0ubmFtZSkKICAgICAgbGV0IG1hcEsgPSBkYXRhTmFtZU1hcFtpdGVtLm5hbWVdOwogICAgICBzZWxlY3RzW2l0ZW0ubmFtZV0gPSB0cnVlOwogICAgICBsZXQgX2NvbG9yID0gY29sb3JzW2luZGV4XQoKICAgICAgbGV0IGljb24gPSBgaWNvbl9gICsgbWFwSwogICAgICBsZWdlbmRSaWNoc1tpY29uXSA9IHsKICAgICAgICAgd2lkdGg6IDE0LAogICAgICAgICBoZWlnaHQ6IDE0LAogICAgICAgICBiYWNrZ3JvdW5kQ29sb3I6IF9jb2xvciwKICAgICAgICAgYm9yZGVyUmFkaXVzOiAxNCwKICAgICAgfQoKICAgICAgbGV0IHZhbHVlMSA9ICd2YWx1ZTFfJyArIG1hcEs7CiAgICAgIGxldCB2YWx1ZTIgPSAndmFsdWUyXycgKyBtYXBLOwogICAgICBsZWdlbmRSaWNoc1t2YWx1ZTFdID0gewogICAgICAgICB3aWR0aDogMTIwLAogICAgICAgICBmb250U2l6ZTogMTgsCiAgICAgICAgIGNvbG9yOiBfY29sb3IsCiAgICAgICAgIHBhZGRpbmc6IFswLCAwLCAwLCAxNCArIDRdCiAgICAgIH0KICAgICAgbGVnZW5kUmljaHNbdmFsdWUyXSA9IHsKICAgICAgICAgZm9udFNpemU6IDE4LAogICAgICAgICBjb2xvcjogX2NvbG9yLAogICAgICB9CiAgIH0pCn0KCmNvbXB1dGVkTGVnZW5kUmljaHMoZGF0YSk7CgpvcHRpb24gPSB7CiAgIC8v5L2g55qE5Luj56CBCiAgIGNvbG9yOiBjb2xvcnMsCiAgIGJhY2tncm91bmRDb2xvcjogIiNmZmYiLAogICBzZXJpZXM6IFsKICAgICAgewogICAgICAgICB0eXBlOiAncGllJywKICAgICAgICAgcmFkaXVzOiBbJzMwJScsICczNSUnXSwKICAgICAgICAgY2VudGVyOiBbJzYwJScsICc1MCUnXSwKICAgICAgICAgbGFiZWw6IHsKICAgICAgICAgICAgc2hvdzogZmFsc2UsCiAgICAgICAgIH0sCiAgICAgICAgIGRhdGE6IGRhdGEsCiAgICAgICAgIGl0ZW1TdHlsZTogewogICAgICAgICAgICBib3JkZXJXaWR0aDogMiwKICAgICAgICAgICAgYm9yZGVyQ29sb3I6ICcjZmZmJwogICAgICAgICB9LAogICAgICAgICBtaW5BbmdsZTogMwogICAgICB9CiAgIF0sCiAgIGxlZ2VuZDogWwogICAgICB7CiAgICAgICAgIG9yaWVudDogJ3ZlcnRpY2FsJywKICAgICAgICAgbGVmdDogJzE1JScsCiAgICAgICAgIHRvcDogJ2NlbnRlcicsCiAgICAgICAgIGljb246ICdjaXJjbGUnLAogICAgICAgICBmb3JtYXR0ZXI6IChsZWcpID0+IHsKICAgICAgICAgICAgbGV0IGl0ZW0gPSBkYXRhLmZpbHRlcigoVikgPT4gVi5uYW1lID09IGxlZylbMF07CiAgICAgICAgICAgIGxldCBtYXBLID0gZGF0YU5hbWVNYXBbbGVnXTsKICAgICAgICAgICAgbGV0IGljb24gPSAnaWNvbl8nICsgbWFwSzsKICAgICAgICAgICAgbGV0IHZhbHVlMSA9ICd2YWx1ZTFfJyArIG1hcEs7CiAgICAgICAgICAgIGxldCB2YWx1ZTIgPSAndmFsdWUyXycgKyBtYXBLOwogICAgICAgICAgICBpZiAoc2VsZWN0c1tsZWddKSB7CiAgICAgICAgICAgICAgIHJldHVybiBgeyR7aWNvbn18fXt0aXRsZTF8JHtsZWd9fXt0aXRsZTJ85Y2g5q+UfVxue3poYW58fVxueyR7dmFsdWUxfXwke2l0ZW0udmFsdWV9fXske3ZhbHVlMn18JHtwdXNoWmVybyhpdGVtLnBybyl9JX1gCiAgICAgICAgICAgIH0gZWxzZSB7CiAgICAgICAgICAgICAgIC8vIGljb27ngbDoibIKICAgICAgICAgICAgICAgcmV0dXJuIGB7bnNlbHx9e3RpdGxlMXwke2xlZ319e3RpdGxlMnzljaDmr5R9XG57emhhbnx9XG57JHt2YWx1ZTF9fCR7aXRlbS52YWx1ZX19eyR7dmFsdWUyfXwke3B1c2haZXJvKGl0ZW0ucHJvKX0lfWAKCiAgICAgICAgICAgICAgIC8vIOWPlua2iOmAieaLqeeahOWFqOeBsOiJsgogICAgICAgICAgICAgICAvLyByZXR1cm4gYHtuc2VsfH17dGl0bGUxfCR7bGVnfX17dGl0bGUyfOWNoOavlH1cbnt6aGFufH1cbntudmFsdWUxfCR7aXRlbS52YWx1ZX19e252YWx1ZTJ8JHtwdXNoWmVybyhpdGVtLnBybyl9JX1gCiAgICAgICAgICAgIH0KICAgICAgICAgfSwgIC8vMTQgKyAxNCArIDE4CiAgICAgICAgIGl0ZW1XaWR0aDogMCwKICAgICAgICAgaXRlbUhlaWdodDogMCwKICAgICAgICAgcGFkZGluZzogMCwKICAgICAgICAgaXRlbUdhcDogMzAsCiAgICAgICAgIGRhdGE6IGxlZ2VuZCwKICAgICAgICAgdGV4dFN0eWxlOiB7CiAgICAgICAgICAgIHJpY2g6IGxlZ2VuZFJpY2hzCiAgICAgICAgIH0KICAgICAgfQogICBdCn07CgpteUNoYXJ0Lm9uKCdsZWdlbmRzZWxlY3RjaGFuZ2VkJywgKGV2ZW50KSA9PiB7CiAgIHNlbGVjdHMgPSBldmVudC5zZWxlY3RlZDsKICAgbXlDaGFydC5zZXRPcHRpb24oewogICAgICBsZWdlbmQ6IHt9CiAgIH0pCn0p`
+let Base64 = {
+   _keyStr: "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/=",
 
-let valueAxis = [
-   {
-      type: 'value',
-      splitNumber: 10,      //分割段数 就是展示 10个 y的label刻度值
-      splitLine: {
-         show: true,
-         lineStyle: {
-            color: ['#cdd2de'],
-            type: 'dashed'
+   encode: function (e: string) {
+      let t = "";
+      let n, r, i, s, o, u, a;
+      let f = 0;
+      e = Base64._utf8_encode(e);
+      while (f < e.length) {
+         n = e.charCodeAt(f++);
+         r = e.charCodeAt(f++);
+         i = e.charCodeAt(f++);
+         s = n >> 2;
+         o = (n & 3) << 4 | r >> 4;
+         u = (r & 15) << 2 | i >> 6;
+         a = i & 63;
+         if (isNaN(r)) {
+            u = a = 64
+         } else if (isNaN(i)) {
+            a = 64
          }
-      },
-      axisLine: {
-         show: false
-      },
-      axisTick: {
-         show: false
+         t = t + this._keyStr.charAt(s) + this._keyStr.charAt(o) + this._keyStr.charAt(u) + this._keyStr.charAt(a)
       }
+      return t
    },
-   {
-      type: 'value',
-      splitNumber: 10,
-      axisLine: {
-         show: false
-      },
-      axisTick: {
-         show: false
-      },
-      splitLine: {
-         show: true,
-         lineStyle: {
-            color: ['#cdd2de'],
-            type: 'dashed'
+
+   decode: function (e: string) {
+      let t = "";
+      let n, r, i;
+      let s, o, u, a;
+      let f = 0;
+      e = e.replace(/[^A-Za-z0-9+/=]/g, "");
+      while (f < e.length) {
+         s = this._keyStr.indexOf(e.charAt(f++));
+         o = this._keyStr.indexOf(e.charAt(f++));
+         u = this._keyStr.indexOf(e.charAt(f++));
+         a = this._keyStr.indexOf(e.charAt(f++));
+         n = s << 2 | o >> 4;
+         r = (o & 15) << 4 | u >> 2;
+         i = (u & 3) << 6 | a;
+         t = t + String.fromCharCode(n);
+         if (u != 64) {
+            t = t + String.fromCharCode(r)
+         }
+         if (a != 64) {
+            t = t + String.fromCharCode(i)
          }
       }
+      t = Base64._utf8_decode(t);
+      return t
+   },
+
+   _utf8_encode: function (e: string) {
+      e = e.replace(/rn/g, "n");
+      let t = "";
+      for (let n = 0; n < e.length; n++) {
+         let r = e.charCodeAt(n);
+         if (r < 128) {
+            t += String.fromCharCode(r)
+         } else if (r > 127 && r < 2048) {
+            t += String.fromCharCode(r >> 6 | 192);
+            t += String.fromCharCode(r & 63 | 128)
+         } else {
+            t += String.fromCharCode(r >> 12 | 224);
+            t += String.fromCharCode(r >> 6 & 63 | 128);
+            t += String.fromCharCode(r & 63 | 128)
+         }
+      }
+      return t
+   },
+
+   _utf8_decode: function (e: string) {
+      let t = "";
+      let n = 0;
+      let r: number = 0;
+      let c1: number = 0;
+      let c2: number = 0;
+      let c3 = null;
+      while (n < e.length) {
+         r = e.charCodeAt(n);
+         if (r < 128) {
+            t += String.fromCharCode(r);
+            n++
+         } else if (r > 191 && r < 224) {
+            c2 = e.charCodeAt(n + 1);
+            t += String.fromCharCode((r & 31) << 6 | c2 & 63);
+            n += 2
+         } else {
+            c2 = e.charCodeAt(n + 1);
+            c3 = e.charCodeAt(n + 2);
+            t += String.fromCharCode((r & 15) << 12 | (c2 & 63) << 6 | c3 & 63);
+            n += 3
+         }
+      }
+      return t
    }
-]
-
-let categoryAxis = [
-   {
-      type: 'category',
-      data: data.xdata,
-   },
-]
-
-let colors = [
-   {
-      type: 'linear',
-      x: 0,
-      y: 0,
-      x2: 0,
-      y2: 1,
-      colorStops: [{
-         offset: 0, color: '#fec51d' // 0% 处的颜色
-      }, {
-         offset: 1, color: '#f33826' // 100% 处的颜色
-      }],
-      global: false // 缺省为 false
-   },
-   '#cb75f1'
-]
-option = {
-   title: {
-      text: '{switchIcon|}',
-      textStyle: {
-         rich: {
-            switchIcon: {
-               width: 32,
-               height: 32,
-               backgroundColor: {
-                  image: switchIcon,
-                  replace: 'no-replace'
-               }
-            }
-         }
-      }
-   },
-   backgroundColor: "#fff",
-   legend: {},
-   tooltip: {
-      trigger: 'axis',
-      show: true,
-   },
-   color: colors,
-   xAxis: categoryAxis,
-   yAxis: valueAxis,
-   series: [
-      {
-         type: 'bar',
-         name: '发展量',
-         barWidth: 20,
-         id: 'valueBar',
-         data: data.develop,
-      },
-
-      {
-         type: 'line',
-         name: '投诉量',
-         data: data.complaint,
-         smooth: true,
-         symbolSize: 10,
-         yAxisIndex: 1,
-         id: 'valueLine',
-         lineStyle: {
-            type: 'solid',
-            width: 3,
-         }
-      },
-   ]
 };
-
-let orient = 'horizontal';
-const orientOfData = (_o) => {
-   let option_ = myChart.getOption();
-   myChart.clear();
-   requestAnimationFrame(() => {
-      if (_o == 'horizontal') {
-         categoryAxis[0]['axisLabel']={
-            rotate:0
-         }
-         option_.yAxis = valueAxis;
-         option_.xAxis = categoryAxis;
-         option_.series[0].xAxisIndex = 0;
-         option_.series[0].yAxisIndex = 0;
-         option_.series[1].xAxisIndex = 0;
-         option_.series[1].yAxisIndex = 1;
-         myChart.setOption(option_)
-      } else if (_o == 'vertical') {
-         categoryAxis[0]['axisLabel']={
-            rotate:-90
-         }
-         option_.yAxis = categoryAxis;
-         option_.xAxis = valueAxis;
-         option_.series[0].xAxisIndex = 0;
-         option_.series[0].yAxisIndex = 0;
-         option_.series[1].xAxisIndex = 1;
-         option_.series[1].yAxisIndex = 0;
-         myChart.setOption(option_)
-      }
-   })
-
-}
-
-
-let orientMap = {
-   'horizontal': 'vertical',
-   'vertical': 'horizontal'
-}
-
-myChart.getZr().on('click', (e) => {
-   let flat = e.topTarget?.parent?.style?.text == '{switchIcon|}';
-   if (flat) {
-      orient = orientMap[orient]
-      orientOfData(orient)
-   }
-})`
+export const code: string =  Base64.decode(_str);
